d29a4cdf394fb201dab429784cc2782284d54129
twant <taylor.want@gmail.com>
2016-10-17 18:20:06 +0000

    first commit

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+#start by coding your methods here! The addition method has been done for you!
+def addition(x,y)
+  x+y
+end
+
+
+
+
+
+#Ask the user for input here!
+
+
+
+
+
+
+#Call your methods to print out the correct answer to the console!
